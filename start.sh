e52ec3518fdd37ff679d6242e82501c4044e1e97
Ramesh Balaji <post2rb@hotmail.com>
2020-09-04 11:27:11 -0400

    Add supervisord

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gunicorn --bind 0.0.0.0:5000 emotes.wsgi:app &
-celery -A emotes.wsgi.celery -c 1 worker
+gunicorn --bind 0.0.0.0:5000 emotes.wsgi:app
+# celery -A emotes.wsgi.celery -c 1 worker
 
-fg %1
+# fg %1
