883b3c59437562193febf7da577f794dae9fbcda
Ramesh Balaji <post2rb@hotmail.com>
2021-01-29 18:23:03 -0500

    Fix global namespace migration *again*

--- migrator/migrations/global_db_namespace_20210129_135137.py
+++ migrator/migrations/global_db_namespace_20210129_135137.py
@@ -7,7 +7,7 @@ def migration_20210129_135137():
     """Put your migration here."""
     print("Here")
 
-    admin = User.get(User.admin == 1).first()
+    admin = User.get(User.admin == 1)
 
     global_nmsp = Namespace(name="Global", slug="", user_id=admin.id)
     global_nmsp.save()
